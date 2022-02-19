4962f8327abe2d897c7b6fcf523113abeab72c96
Darren Norris <doon75@hotmail.com>
2022-02-19 15:01:50 -0300

    GAM exploratory analysis

--- R/gdp_analysis.R
+++ R/gdp_analysis.R
@@ -2,6 +2,11 @@ library(tidyverse)
 library(readxl)
 library(scales)
 library(mgcv)
+library(stringi)
+
+
+#google: r cran purtest example tutorial interpretation
+#stationarity https://kevinkotze.github.io/ts-6-tut/
 df_muni <- read_excel("data//bla_municipalities.xlsx", 
                                    na = c("", "NA"),
                                    sheet = "municipality_fixed_ref",
@@ -12,7 +17,6 @@ df_muni_year <- read_excel("data//bla_municipalities.xlsx",
                       .name_repair = "universal")
 
 #Basic reference vectors
-#Not obvious how to select state capitals
 bla_state_names <- c("Acre", "Amapá", "Amazonas", "Maranhão", 
                      "Mato Grosso", "Pará", "Tocantins", "Rondônia", "Roraima")
 bla_state_siglas <- c("AC", "AP", "AM", "MA", 
@@ -202,6 +206,28 @@ dev.off()
 
 
 #Annual patterns
+#757 municipalities
+df_muni_year %>% 
+  select(muni_name, muni_area_km2, year, gdp_percapita_reais, tot_loss_percent, 
+         school_per1000, superior_course_per1000, pg_per1000) %>% 
+  filter(!is.na(tot_loss_percent), !is.na(school_per1000)) %>% 
+  group_by(muni_name, muni_area_km2) %>% 
+  summarise(acount = n(), 
+            count_year = length(unique(year))) %>% 
+  arrange(desc(count_year)) %>% 
+  ungroup() %>% pull(muni_area_km2) %>% sum() #[1] 5030055
+
+df_muni_year %>% 
+  select(state_name, muni_name, muni_area_km2, year, 
+         gdp_percapita_reais, tot_loss_percent, 
+         school_per1000, superior_course_per1000, pg_per1000) %>% 
+  filter(!is.na(tot_loss_percent), !is.na(school_per1000)) %>% 
+  ggplot(aes(x=year, y = school_per1000)) + 
+  geom_point() + 
+  #stat_smooth(method="gam") + 
+  stat_smooth(aes(colour = state_name), method="gam")
+
+
 df_muni_year %>%
 ggplot(aes(x=gva_agri_percapita_reais/3.946, y=gdp_percapita_reais/3.946)) + 
   geom_point(aes(colour=factor(year))) + 
@@ -391,3 +417,158 @@ df_muni %>%
   labs(title = "GDP and forest cover 2019", 
        x = "school pass rate") + 
   theme(plot.title.position = "plot") 
+
+#models
+#mgcv timeseries
+#https://petolau.github.io/Analyzing-double-seasonal-time-series-with-GAM-in-R/
+#https://fromthebottomoftheheap.net/2014/05/09/modelling-seasonal-data-with-gam/
+#https://fromthebottomoftheheap.net/2021/02/02/random-effects-in-gams/
+
+var_response <- c("gdp_percapita_reais")
+var_timeconstant <- c("state_name", "muni_name", "muni_area_km2", "dist_statecapital_km", 
+         "flag_urban")
+var_timevary <- c("year","pop_dens_km2", "tot_loss_percent", "gva_agri_percapita_reais",
+                  "school_per1000", "superior_course_per1000", "pg_per1000")
+var_lags <- c("lag01_lossarea_per", "lag02_lossarea_per", "lag03_lossarea_per", 
+              "lag04_lossarea_per", "lag05_lossarea_per", "lag06_lossarea_per", 
+              "lag07_lossarea_per", "lag08_lossarea_per", "lag09_lossarea_per", 
+              "lag10_lossarea_per")
+df_muni_year %>% 
+  filter(!is.na(tot_loss_percent), !is.na(school_per1000), 
+         !is.na(superior_course_per1000), !is.na(pg_per1000), 
+         dist_statecapital_km >0) %>% 
+  select(all_of(var_response), all_of(var_timeconstant), all_of(var_timevary), 
+         all_of(var_lags)) %>% 
+  mutate(tot_loss3y_percent = lag01_lossarea_per + 
+           lag02_lossarea_per + lag03_lossarea_per, 
+         tot_loss5y_percent = lag01_lossarea_per + 
+           lag02_lossarea_per + lag03_lossarea_per + 
+           lag04_lossarea_per + lag05_lossarea_per, 
+         adate = as.Date(paste(year,"-01", "-01", sep="")), 
+         format = c("%Y-%m-%d")) -> dfgam
+which(is.na(dfgam)[,3]) #
+dfgam$muni_namef <- as.factor(dfgam$muni_name) 
+dfgam$state_namef <- as.factor(dfgam$state_name)
+
+#library(corrgram) -18 (lag 1,2,3)
+corrgram(dfgam[, c(var_response, "tot_loss_percent", var_lags)],
+         lower.panel=panel.pts, upper.panel=panel.conf,
+         diag.panel=panel.density)
+cor.test(dfgam$gdp_percapita_reais, 
+         dfgam$school_per1000) #0.07020494
+cor.test(dfgam$gdp_percapita_reais, 
+         dfgam$superior_course_per1000) #- 0.004 NS
+cor.test(dfgam$gdp_percapita_reais, 
+         dfgam$pg_per1000) #0.078
+
+#Model
+#Need to use log as there are extreme outlier gdp_percapita values 
+model_00 <- gam(log(gdp_percapita_reais) ~ year +
+                  s(year, state_namef,  k=5, bs="fs", m=2) + 
+                  s(gva_agri_percapita_reais, state_namef, 
+                    k=5, bs="fs", m=2) +
+                  s(school_per1000) + 
+                   s(pg_per1000) + 
+                  s(dist_statecapital_km, state_namef, 
+                    k=5, bs="fs", m=2) +
+                  s(tot_loss5y_percent), 
+                 data = dfgam, 
+                method="REML")
+gam.check(model_00)
+summary(model_00)
+
+#
+ctrl <- list(niterEM = 0, msVerbose = TRUE, optimMethod="L-BFGS-B")
+model_01 <- gamm(log(gdp_percapita_reais) ~ year +
+                   s(year, state_namef,  k=5, bs="fs", m=2) + 
+                   s(gva_agri_percapita_reais, state_namef, 
+                     k=5, bs="fs", m=2) +
+                   s(school_per1000) + 
+                   s(pg_per1000) + 
+                   s(dist_statecapital_km, state_namef, 
+                     k=5, bs="fs", m=2) +
+                   s(tot_loss5y_percent), 
+                 data = dfgam, 
+                 method="REML")
+summary(model_01$lme)
+
+#AR2 2 hours or so
+model_01_ar2 <- gamm(gdp_percapita_reais ~ year + s(school_per1000) + 
+                       s(pg_per1000) + s(tot_loss5y_percent) + 
+                       s(gva_agri_percapita_reais) +
+                       s(dist_statecapital_km), 
+                     data = dfgam, 
+                     correlation = corARMA(form = ~ 1|year, p = 2), 
+                     control = ctrl)
+
+#AR4 2 hours or so
+model_01_ar4 <- gamm(gdp_percapita_reais ~ year + s(school_per1000) + 
+                   s(pg_per1000) + s(tot_loss5y_percent) + 
+                     s(gva_agri_percapita_reais) +
+                   s(dist_statecapital_km), 
+                 data = dfgam, 
+                   correlation = corARMA(form = ~ 1|year, p = 4), 
+                 control = ctrl)
+summary(model_01_ar4$lme)
+
+#residuals
+anova(model_01$lme, model_01_ar4$lme)
+res_gam <- resid(model_00, type = "deviance")
+res_gamm <- resid(model_01$lme, type = "normalized")
+res_gamm_ar3 <- resid(model_01_ar4$lme, type = "normalized")
+
+dfgam$m01_res_ar3 <- res_ar3
+dfgam$m01_res_gam <- res_gam
+dfgam$m01_res_gamm <- res_gamm
+
+library(timetk)
+dfgam %>%
+  group_by(state_name, muni_name) %>%
+  tk_acf_diagnostics(
+    .date_var = year,
+    .value = m01_res_gamm, 
+    .lags = 11
+  ) -> tidy_acf
+
+#export as .png  250 * 1000
+tidy_acf %>% 
+  ggplot(aes(x = lag, y = ACF, color = state_name, 
+             group = state_name)) +
+  # Add horizontal line a y=0
+  geom_hline(yintercept = 0) +
+  # Plot autocorrelations
+  geom_point(size = 2) +
+  geom_segment(aes(xend = lag, yend = 0), size = 1) +
+  # Add cutoffs
+  geom_line(aes(y = .white_noise_upper), color = "black", 
+            linetype = 2) +
+  geom_line(aes(y = .white_noise_lower), color = "black", 
+            linetype = 2) +
+  # Add facets
+  facet_wrap(~ state_name, ncol = 1) +
+  # Aesthetics
+  expand_limits(y = c(-1, 1)) +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    plot.title = element_text(hjust = 0.5)
+  ) + labs(
+    title = "AutoCorrelation (ACF)",
+    subtitle = "GAMM residuals", 
+    xlab = "lag (year)"
+  )
+
+#https://www.kaggle.com/janiobachmann/time-series-i-an-introductory-start/script
+dfgam %>%
+  group_by(state_name, muni_name) %>% 
+  plot_acf_diagnostics(
+    date,
+    AAPL,
+    .ccf_vars = GOOGL,
+    .show_ccf_vars_only = TRUE,
+    .interactive=FALSE, 
+    .line_color = "black",
+    .point_color =palette_light()[[2]],
+    .line_size = 1.5,
+    .title = "Cross Correlation of Technology Stocks"
+  ) 
