b323073fd6d3e2be8bca50cbee0d921698912096
darrennorris <doon75@hotmail.com>
2022-03-04 19:11:09 -0300

    updated - good residual histogram

--- R/gdp_gams.R
+++ R/gdp_gams.R
@@ -11,7 +11,7 @@ library(sf)
 memory.limit(50000)
 
 #Uses dfgam from "gdp_analysis.R"
-dfgam <- readRDS("dfgam.rds") #13710 obs. 47 vars
+dfgam <- readRDS("dfgam.rds") #13710 obs. 52 vars
 dfgam %>% 
   mutate(flag_gold = factor(if_else(mine_area_km2_gold_percapita >0,1,0))) -> dfgam
 
@@ -64,20 +64,17 @@ hist(resid(model_01_ar1_null$gam, type = "deviance"))
 summary(model_01_ar1_null$gam) #0.744
 
 #Develop 
-model_01 <- gamm(log(gdp_percapita_reais) ~ main_sectorf +
+model_01 <- gamm(log(gdp_percapita_reais) ~ year*dominant_groupsf +
                         s(year, by = state_namef, k=5, m=1, bs="tp") + 
                    s(gva_agri_percapita_reais, by = flag_gold) + 
-                   s(indigenous_area_percent, k=4, by = flag_gold) + 
-                   #s(mine_area_km2_gold_percapita, k=4) + 
-                   #s(mine_area_km2_metal_percapita, k=5) + 
+                   s(indigenous_area_percent, k=4) + 
                    s(process_gold_p1000, k=4) + 
                    s(process_metal_p1000, k=4) + 
-                   s(process_construction_p1000, k=4) +
                    s(tot_loss5y_percent) + 
                    s(pop_dens_km2, k=4) + 
-                   s(school_per1000) + 
+                   #s(state_namef, bs="re") +
                         s(dist_statecapital_km, by = state_namef) + 
-                        s(state_namef, bs="re"), 
+                   s(school_per1000), 
                       data = dfgam, 
                       method="REML")
 hist(resid(model_01$gam, type = "deviance")) #Much improved.
@@ -106,27 +103,26 @@ appraise(model_01$gam) #problems with deviance residual > 1.5
 plot(model_01$gam, scale = 0, all.terms = TRUE)
 
 #with AR working
-model_01_ar1 <- gamm(log(gdp_percapita_reais) ~ main_sectorf +
-                       s(year, by = state_namef, k=5, m=1, bs="tp") + 
+model_01_ar1 <- gamm(log(gdp_percapita_reais) ~ dominant_groupsf2 +
+                       #s(year, by = state_namef, k=5) + 
                        s(gva_agri_percapita_reais, by = flag_gold) + 
-                       s(indigenous_area_percent, k=4, by = flag_gold) + 
+                       s(indigenous_area_percent, k=4) + 
                        s(process_gold_p1000, k=4) + 
                        s(process_metal_p1000, k=4) + 
-                       s(process_construction_p1000, k=4) +
                        s(tot_loss5y_percent) + 
                        s(pop_dens_km2, k=4) + 
-                       s(school_per1000) + 
+                       #s(state_namef, bs="re") +
                        s(dist_statecapital_km, by = state_namef) + 
-                       s(state_namef, bs="re"), 
+                       s(school_per1000), 
                      correlation = corARMA(form = ~ year|muni_factor, p = 1), 
                      data = dfgam, 
                      method="REML", 
                      control = ctrl)
 saveRDS(model_01_ar1, "model_01_ar1.rds")
 model_01_ar1 <- readRDS("model_01_ar1.rds")
-hist(resid(model_01_ar1$gam, type = "deviance"))
+hist(resid(model_01_ar1$gam, type = "deviance")) #histograms good
 summary(model_01_ar1$lme) #check correlation structure
-summary(model_01_ar1$gam) #r2 = 0.874 hist wrse with construction and metal....
+summary(model_01_ar1$gam) #r2 = 0.65....
 
 #gam.check(model_01_ar1$gam) #problem with residual > 1
 appraise(model_01_ar1$gam)
