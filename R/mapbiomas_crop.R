762b801f5a6f34d1d4bc3a1d1472d16a991112a1
darrennorris <doon75@hotmail.com>
2022-04-05 20:37:55 -0300

    update

--- R/mapbiomas_crop.R
+++ R/mapbiomas_crop.R
@@ -8,44 +8,42 @@ library(readxl)
 memory.limit(30000)
 
 #Municipality polygons
-longname <- "vector/brazil_ninestate_municipalities/ninestate_muni.shp"
-sf_munis <- sf::st_read(longname) 
+#longname <- "vector/brazil_ninestate_municipalities/ninestate_muni.shp"
+#sf_munis <- sf::st_read(longname) 
 data.frame(sf_munis) %>% group_by(SIGLA_UF) %>% 
   summarise(area = sum(AREA_KM2)) %>%
   arrange(desc(area))
 
-sf_instate <- st_read("vector//ninestate_poly.shp")
+#sf_instate <- st_read("vector//ninestate_poly.shp")
 #Most large in AM, MT, PA
-data.frame(sf_munis) %>% pull(AREA_KM2) %>% unique() %>% sort()
-data.frame(sf_munis) %>% 
-  filter(AREA_KM2 > 10000, AREA_KM2 < 70000) %>% 
-  group_by(SIGLA_UF) %>% summarise(large_count = n())
+#data.frame(sf_munis) %>% pull(AREA_KM2) %>% unique() %>% sort()
+#data.frame(sf_munis) %>% 
+#  filter(AREA_KM2 > 10000, AREA_KM2 < 70000) %>% 
+#  group_by(SIGLA_UF) %>% summarise(large_count = n())
 
 rm("df_muni_tif")
 mystates <- c("AM", "MT", "PA")
-get_files <- function(folder_name = NA, state_codes = NA) {
+mystates_small <- c("AC", "AP")
+#works one folder at a time
+get_files <- function(folder_name = NA) {
   library(tidyverse)
-folder_location <- paste(folder_name, 
-                         state_codes, sep="")
-tif_files <- list.files(folder_location, 
-                        pattern = ".tif", full.names = TRUE)
-data.frame(folder_id = folder_location, file_id = tif_files) %>% 
-  mutate(state_code = str_sub(folder_id,-2,-1), 
-         ayear = str_sub(tif_files,-8,-5)) %>% 
-  group_by(folder_id, file_id, state_code, ayear) %>% 
-  summarise(file_count = n()) %>% 
-  ungroup() -> df_muni_tif
-return(df_muni_tif)
+  folder_location <- folder_name
+  in_files <- list.files(folder_location, 
+                         pattern = ".tif", full.names = TRUE)
+  data.frame(folder_id = folder_location, file_id = in_files) %>%  
+    group_by(folder_id, file_id) %>% 
+    summarise(file_count = n()) %>% 
+    ungroup() -> df_muni_tif
+  return(df_muni_tif)
 }
-df_muni_tif <- get_files(folder_name = "mapbiomas_ge/state_cover/", 
-          state_codes = mystates)
+infolder <- "mapbiomas_ge/state_cover/RR"
+df_muni_tif <- get_files(folder_name = infolder)
+#update
+df_muni_tif %>% 
+  mutate(state_code = str_sub(folder_id, -2, -1), 
+         ) -> df_muni_tif
 
-rin <- df_muni_tif$file_id[1] 
-rbig <- terra::rast(rin)
-res(rbig) *100000
-crs(rbig)
-
-state_cover <- function(x, state_id = "AM", 
+state_proj <- function(x, state_id = NA, 
                         sf_state = NA) {
   library(plyr)
   library(tidyverse)
@@ -53,7 +51,7 @@ state_cover <- function(x, state_id = "AM",
   library(sf)
   library(stringi)
   
-  state_sigla = state_id 
+  state_sigla = x$state_code 
   rin <- x$file_id 
   rbig <- terra::rast(rin)
   layer_name <- names(rbig)
@@ -66,31 +64,32 @@ state_cover <- function(x, state_id = "AM",
   
   #Crop
   #rtest_mask <- crop(rbig, e2, snap="out")
-  rtest_mask <- rbig
-  rm("rbig") 
+  #rtest_mask <- rbig
+  #rm("rbig") 
   
   #Project
   new_crs <- "+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs"
-  rtest_mask <- project(rtest_mask, new_crs, method = "near") 
-  
+  #rtest_mask <- project(rtest_mask, new_crs, method = "near")
+  rbig <- project(rbig, new_crs, method = "near")
   #Export
   folder <- paste(state_sigla,"_", "equalarea", sep ="")
   folder_path <- paste("mapbiomas_ge/state_cover/", folder, sep = "")
-  outfile <- paste("state_coverage_", state_sigla,"_", 
+  outfile <- paste("ea_cover_", state_sigla,"_", 
                    layer_year, ".tif", sep = "")
   f <- file.path(folder_path, outfile)
-  writeRaster(rtest_mask, f, datatype = "INT2U", overwrite = TRUE)
+  writeRaster(rbig, f, datatype = "INT2U", overwrite = TRUE)
   #writeRaster(rtest_mask, outfile, datatype = "INT2U", overwrite = TRUE)
   
   #clear temporary files
   tmpFiles(current =TRUE, remove = TRUE) 
   
   endtime <- Sys.time() 
-  #return(print(endtime))
+  textout <- paste(outfile, ": ", endtime, sep="")
+  print(textout)
 }
-#run
+#run 14:24
 plyr::a_ply(df_muni_tif[1, ], .margins = 1, 
-            .fun = state_cover)
+            .fun = state_proj)
 
 #parallel example
 library(doParallel)
@@ -98,9 +97,15 @@ library(doParallel)
 cores <- detectCores()
 cores
 usecores <- cores -1
+usecores <- cores # if there is enough free memory etc....
 ## register
 cl <- makeCluster(usecores)
 registerDoParallel(cl)
+#11:08 - 11:20
+#inid <- seq(6, 36, by=2)
+plyr::a_ply(df_muni_tif[2:36, ], .margins = 1, 
+            .fun = state_proj, .parallel = TRUE)
+
 #9:18 - 10:12 #Amazonas = 1 hour
 #10:24 - 11:23 1 hour for 3, 12 hours for 36
 seldone <- which(df_muni_tif$ayear %in% c(2013, 2014, 2015))
